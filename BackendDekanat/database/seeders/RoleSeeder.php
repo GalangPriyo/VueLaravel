361e1c79c94ba2718e577a40caaa32aec40f0a60
Galang Priyohutomo <galangpriyohutomo@students.undip.ac.id>
2024-10-08 13:32:59 +0700

    Second Commit : Video 2 dan 3

---
+++ BackendDekanat/database/seeders/RoleSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Schema::disableForeignKeyConstraints();
+        // Role::truncate();
+        // Schema::enableForeignKeyConstraints();
+
+        // $data = [
+        //     'waitress',
+        //     'chef',
+        //     'cashier',
+        //     'manager'
+        // ];
+
+        // foreach ($data as $role) {
+        //     Role::create([
+        //         'name' => $role,
+        //         'created_at' => now(),
+        //         'updated_at' => now()
+        //     ]);
+        // }
+    }
+}
